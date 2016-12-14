f9a474716ed8e93cf3fb9993ff0adf581968bf8c
bitocreator <bitocreator@gmail.com>
2016-12-14 22:06:19 +0100

    finishing fonts

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,10 @@ module Suppleo
     # -- all .rb files in that directory are automatically loaded.
     
 	# Add assets paths
-    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+    #config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
     config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'javascripts')
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'stylesheets')
 
   end
 end
