6bd57d0b88d929f98675b5e583640ba91d423f94
Mac Thib <bitocreator@gmail.com>
2017-03-11 12:43:21 +0100

    fix email issue on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,7 +8,20 @@ Rails.application.configure do
   config.assets.compile = false
   config.log_level = :debug
   config.log_tags = [ :request_id ]
-  config.action_mailer.perform_caching = false
+
+  config.action_mailer.raise_delivery_errors = false
+    config.action_mailer.perform_caching = false
+    # SMTP settings for gmail
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+     :address              => "smtp.gmail.com",
+     :port                 => 587,
+     :user_name            => ENV['gmail_username'],
+     :password             => ENV['gmail_password'],
+     :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
+
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
